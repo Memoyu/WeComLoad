1fad727b721e51947096700042f7a4192ed9d486
蒙明宇 <mmy6076@outlook.com>
2022-04-06 14:46:46 +0800

    fix：cors

--- src/WeComLoad.Open.Blazor/wwwroot/js/wecom.js
+++ src/WeComLoad.Open.Blazor/wwwroot/js/wecom.js
@@ -5,11 +5,11 @@
 export async function checkLoginState() {
     const data = await fetch("http://127.0.0.1:50000/checkLoginState", {
         headers: {
-            //'accept': '*/*',
-            //'accept-language': 'zh-CN,zh;q=0.9',
-            //'cache-control': 'no-cache',
+            'accept': '*/*',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cache-control': 'no-cache',
             'content-type': 'text/plain;charset=UTF-8',
-            /*'pragma': 'no-cache',
+            'pragma': 'no-cache',
             'sec-ch-ua': '\" Not A;Brand\";v=\'99\", \"Chromium\";v=\'99\", \"Microsoft Edge\";v=\"99\"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': 'Windows',
@@ -18,7 +18,7 @@ export async function checkLoginState() {
             'sec-fetch-site': 'cross-site',
             'Host': '127.0.0.1:50000',
             'Origin': 'https://open.work.weixin.qq.com',
-            'referer': 'https://open.work.weixin.qq.com/',*/
+            'referer': 'https://open.work.weixin.qq.com/',
         },
         referrerPolicy: "strict-origin-when-cross-origin",
         body: "{\"scene\":1,\"redirect_uri\":\"https://open.work.weixin.qq.com\"}",
@@ -38,11 +38,11 @@ export async function confirmLogin(key) {
     }
     const respo = await fetch("http://127.0.0.1:50000/confirmLogin", {
         headers: {
-            //'accept': '*/*',
-            //'accept-language': 'zh-CN,zh;q=0.9',
-            //'cache-control': 'no-cache',
+            'accept': '*/*',
+            'accept-language': 'zh-CN,zh;q=0.9',
+            'cache-control': 'no-cache',
             'content-type': 'text/plain;charset=UTF-8',
-            /*'pragma': 'no-cache',
+            'pragma': 'no-cache',
             'sec-ch-ua': '\" Not A;Brand\";v=\'99\", \"Chromium\";v=\'99\", \"Microsoft Edge\";v=\"99\"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': 'Windows',
@@ -51,7 +51,7 @@ export async function confirmLogin(key) {
             'sec-fetch-site': 'cross-site',
             'Host': '127.0.0.1:50000',
             'Origin': 'https://open.work.weixin.qq.com',
-            'referer': 'https://open.work.weixin.qq.com/',*/
+            'referer': 'https://open.work.weixin.qq.com/',
         },
         referrerPolicy: "strict-origin-when-cross-origin",
         body: JSON.stringify(data),
