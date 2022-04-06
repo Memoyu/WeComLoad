0a42d9df312c10f31b46a8ff724ef09cf6bc1150
蒙明宇 <mmy6076@outlook.com>
2022-04-06 16:29:08 +0800

    fix：cors error

--- src/WeComLoad.Open.Blazor/wwwroot/js/wecom.js
+++ src/WeComLoad.Open.Blazor/wwwroot/js/wecom.js
@@ -6,20 +6,18 @@ export async function checkLoginState() {
     const data = await fetch("http://127.0.0.1:50000/checkLoginState", {
         headers: {
             'content-type': 'text/plain;charset=UTF-8',
-            'mode': 'no-cors',
             'accept': '*/*',
             'accept-language': 'zh-CN,zh;q=0.9',
-            'cache-control': 'no-cache',
-            'pragma': 'no-cache',
-            /*'sec-ch-ua': '\" Not A;Brand\";v=\'99\", \"Chromium\";v=\'99\", \"Microsoft Edge\";v=\"99\"',
+            //'pragma': 'no-cache',
+            'sec-ch-ua': '\" Not A;Brand\";v=\'99\", \"Chromium\";v=\'99\", \"Microsoft Edge\";v=\"99\"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': 'Windows',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',*/
+            'sec-fetch-site': 'cross-site',
             'Host': '127.0.0.1:50000',
-            'Origin': 'https://open.work.weixin.qq.com',
-            'referer': 'https://open.work.weixin.qq.com/',
+            'Origin': 'http://localhost:5000',
+            'referer': 'https://open.work.weixin.qq.com/',//*/
         },
         // referrerPolicy: "strict-origin-when-cross-origin",
         body: "{\"scene\":1,\"redirect_uri\":\"https://open.work.weixin.qq.com\"}",
@@ -40,20 +38,18 @@ export async function confirmLogin(key) {
     const respo = await fetch("http://127.0.0.1:50000/confirmLogin", {
         headers: {
             'content-type': 'text/plain;charset=UTF-8',
-            'mode': 'no-cors',
             'accept': '*/*',
             'accept-language': 'zh-CN,zh;q=0.9',
-            'cache-control': 'no-cache',
-            'pragma': 'no-cache',
-            /*'sec-ch-ua': '\" Not A;Brand\";v=\'99\", \"Chromium\";v=\'99\", \"Microsoft Edge\";v=\"99\"',
+            //'pragma': 'no-cache',
+            'sec-ch-ua': '\" Not A;Brand\";v=\'99\", \"Chromium\";v=\'99\", \"Microsoft Edge\";v=\"99\"',
             'sec-ch-ua-mobile': '?0',
             'sec-ch-ua-platform': 'Windows',
             'sec-fetch-dest': 'empty',
             'sec-fetch-mode': 'cors',
-            'sec-fetch-site': 'cross-site',*/
+            'sec-fetch-site': 'cross-site',
             'Host': '127.0.0.1:50000',
             'Origin': 'https://open.work.weixin.qq.com',
-            'referer': 'https://open.work.weixin.qq.com/',
+            'referer': 'https://open.work.weixin.qq.com/', //*/
         },
         // referrerPolicy: "strict-origin-when-cross-origin",
         body: JSON.stringify(data),
